5ae757d44d8dafd34215eb5a17ba43ba0af927e6
fairyscript <h@queb.fun>
2021-12-29 22:56:10 +0800

    微调log

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs'
-
 import { Config } from './types/config'
 import { getLogger } from 'log4js'
 
@@ -9,8 +8,9 @@ export async function getConfig(): Promise<Config> {
     const config = (await import('../config/config')).default
     return config
   } catch (error) {
-    logger.warn('load config fail! try copy example Config')
+    logger.warn('load config fail! load example config')
     if (!fs.existsSync('./config/config.ts')) {
+      logger.info('try copy example Config')
       fs.copyFileSync('./config/exampleConfig.ts', './config/config.ts')
     }
     return (await import('../config/exampleConfig')).default
